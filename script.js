a93c172b0db9db5a174240785ab69c17b6353fe1
SWETA2318 <150820032+SWETA2318@users.noreply.github.com>
2023-12-27 11:58:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+// Function to validate an email address
 function validEmail(str) {
-  //your JS code here.
+  // Regular expression for a basic email validation
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+  // Check if the email matches the regular expression
+  return emailRegex.test(str);
 }
 
 // Do not change the code below.
